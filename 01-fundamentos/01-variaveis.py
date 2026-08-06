c9cb074e4102a6b756f87a4be124b4dd6adea923
CrystyanDev <crystyanlbn1@gmail.com>
2026-08-06 17:10:58 -0300

    Update 01-variaveis.py

--- 01-fundamentos/01-variaveis.py
+++ 01-fundamentos/01-variaveis.py
@@ -1,8 +1,7 @@
-__name__  = input("Qual é o seu nome") 
+__name__  = input("Qual é o seu nome?") 
 age = input("Qual é a sua idade?")    
 city = input("Qual é a sua cidade?")
 profession = input("Qual é a sua profissão?")
-
 
 
 print (f"Olá, {__name__}")
